65a1691d646e30348418371a76a5ee640fe0804f
naomi <iam.naomi92@gmail.com>
2021-05-07 21:21:54 +0900

    challenge6

--- src/lib/PasswordValidator.js
+++ src/lib/PasswordValidator.js
@@ -67,7 +67,7 @@ export default class extends BaseValidator {
       return Promise.reject({
         success: false,
         type: this.type,
-        message: `${this.typeName}は半角英数字の小文字と数字を1文字以上含めてください。`
+        message: `${this.typeName}は半角英数字の小文字,数字を1文字以上含めてください。`
       })
     }
   }
